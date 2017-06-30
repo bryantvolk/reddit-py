10a3383e2b1bb6dcd9dc5a66b384c504466c86fe
Bryant Volk <bryantvolk96@gmail.com>
2017-06-30 15:43:27 -0400

    basic bot created, stores remembered post id in .txt

--- reddit-py/salesbot/salesbot.py
+++ reddit-py/salesbot/salesbot.py
@@ -1,4 +1,28 @@
 import praw
-
+import re
+import os
+keywords = {'GPU', 'Monitor', 'Keyboard'}
+combined = '(' + ')|('.join(keywords) + ')'
 reddit = praw.Reddit('bot1')
+user = reddit.redditor('annesocks')
+# read databse containing id's for each submission
+if not os.path.isfile('sale_ids.txt'):
+    sale_ids = []
+else:
+    with open('sale_ids.txt', 'r') as f:
+        sale_ids = f.read()
+        sale_ids = sale_ids.split('\n')
+        sale_ids = list(filter(None, sale_ids))
+
 subreddit = reddit.subreddit('buildapcsales')
+for submission in subreddit.new(limit=10):
+    if submission.id not in sale_ids:
+        result = re.search(combined, submission.title, re.IGNORECASE)
+        if result:
+            print(submission.title)
+            user.message(result.group(0).upper(),submission.title + '\n' + submission.shortlink)
+            sale_ids.append(submission.id)
+
+with open('sale_ids.txt', 'w') as f:
+    for sale in sale_ids:
+        f.write(sale + '\n')
